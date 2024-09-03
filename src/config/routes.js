a994e0d92ce4e31ebc77135cc475cfda776574e3
minhha2k3 <hadep7a@gmail.com>
2024-09-03 08:00:14 +0700

    feat: add screen comment and reply comment

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,29 @@
 const routes = {
     home: '/',
-    post: '/posts',
+    blog: '/blog',
     live: '/live',
-    profile: '/:nickname',
+    profile:(slug) => `/${slug}`,
     upload: '/upload',
     search: '/search',
+
+    courseDetails: (course_id) => `course/${course_id}`,
+
+
+    myCourseDetails: (course_id) => `my-course/${course_id}`,
+
+    registeredCourse: '/registered-course',
+    myCourse: '/my-course',
+    createCourse: '/new-course/',
+    roadmap: '/roadmap',
+    bookmark: '/me/bookmark',
+    myBlog: '/me/posts',
+    createBlog: '/new-post',
+    blogDetails:(id) => `/blog/${id}`,
+    editBlog:(id) => `/posts/${id}/edit`,
+    
+    loginEmail: '/login/email',
+    registerEmail: '/register/email',
+    forgotPassword: '/forgot-password',
 }
 
 export default routes
